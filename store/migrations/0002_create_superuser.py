30049170fe5a56c1d3aa10ccffc63d1cc9247a5a
anto-d88 <antonio-123@hotmail.be>
2025-11-23 17:32:19 +0100

    Add clean superuser migration

--- store/migrations/0002_create_superuser.py
+++ store/migrations/0002_create_superuser.py
@@ -1,22 +1,18 @@
 from django.db import migrations
-from django.contrib.auth.hashers import make_password
 
 def create_superuser(apps, schema_editor):
-    User = apps.get_model('auth', 'User')
+    from django.contrib.auth.models import User
     if not User.objects.filter(username='antonio').exists():
-        User.objects.create(
+        User.objects.create_superuser(
             username='antonio',
             email='antonio@example.com',
-            password=make_password('Antonio12345'),
-            is_superuser=True,
-            is_staff=True,
+            password='Antonio12345'
         )
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('store', '0001_initial'),
-        ('auth', '0012_alter_user_first_name_max_length'),
     ]
 
     operations = [
