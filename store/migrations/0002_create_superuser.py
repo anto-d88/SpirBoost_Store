0a61544597949ae6cdb37cefc03d312c02888a2b
anto-d88 <antonio-123@hotmail.be>
2025-11-23 17:45:35 +0100

    Reset store migrations

--- store/migrations/0002_create_superuser.py
+++ store/migrations/0002_create_superuser.py
@@ -1,20 +1,29 @@
 from django.db import migrations
+from django.contrib.auth.hashers import make_password
 
 def create_superuser(apps, schema_editor):
-    from django.contrib.auth.models import User
-    if not User.objects.filter(username='antonio').exists():
-        User.objects.create_superuser(
-            username='antonio',
-            email='antonio@example.com',
-            password='Antonio12345'
+    User = apps.get_model("auth", "User")
+    if not User.objects.filter(username="antonio").exists():
+        User.objects.create(
+            username="antonio",
+            email="antonio@example.com",
+            password=make_password("Antonio12345"),
+            is_superuser=True,
+            is_staff=True,
+            is_active=True
         )
+
+def remove_superuser(apps, schema_editor):
+    User = apps.get_model("auth", "User")
+    User.objects.filter(username="antonio").delete()
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('store', '0001_initial'),
+        ("store", "0001_initial"),
+        ("auth", "0012_alter_user_first_name_max_length"),
     ]
 
     operations = [
-        migrations.RunPython(create_superuser),
+        migrations.RunPython(create_superuser, remove_superuser),
     ]
